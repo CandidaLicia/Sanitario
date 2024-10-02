e79431182dbf949b1037c5d56d7e36925833ee79
Candida Licia Choque Machaca <candy1licia@gmail.com>
2024-10-01 21:27:42 -0400

    Agregar archivos de proyecto.

---
+++ Tables/Detalle_Visita.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Detalle_Visita] (
+    [Id_Detalle_Visita] INT IDENTITY(1,1) NOT NULL,
+	[Id_Visita] INT NOT NULL,
+    [Id_HSM] INT NOT NULL,
+    [Precio_total] FLOAT NULL,
+	[Diagnostico] FLOAT NULL,
+	[datos_adicionales] FLOAT NULL,
+    [Id_Cama] INT NOT NULL,
+	[Es_Internacion] Int NULL,
+    [Fecha_Ingreso] DATETIME NOT NULL,
+    [Fecha_Salida] DATETIME NULL,
+    CONSTRAINT [PK_Detalle_Visita] PRIMARY KEY CLUSTERED ([Id_Detalle_Visita] ASC),
+    --CONSTRAINT [FK_HSM_Visita] FOREIGN KEY ([Id_HSM]) REFERENCES [dbo].[Medico_Hospital_Servicio] ([Id_Medico_Hospital_Servicio]),
+	CONSTRAINT [FK_Visita] FOREIGN KEY ([Id_Visita]) REFERENCES [dbo].[Visita] ([Id_Visita]),
+    CONSTRAINT [FK_Detalle_Visita_Cama] FOREIGN KEY ([Id_Cama]) REFERENCES [dbo].[Cama] ([Id_Cama])
+);
---
+++ Tables/Estudio.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Estudio] (
+    [Id_Estudio] INT IDENTITY(1,1) NOT NULL,
+	[Id_Detalle_Visita] INT NOT NULL,
+   	[Precio] NVARCHAR(150) NOT NULL,
+	[Fecha_Estudio] DATETIME NOT NULL,
+    [Fecha_Entrega] DATETIME NULL,
+    CONSTRAINT [PK_Estudio] PRIMARY KEY CLUSTERED ([Id_Estudio] ASC),
+	CONSTRAINT [FK_Detalle_Visita] FOREIGN KEY ([Id_Detalle_Visita]) REFERENCES [dbo].[Detalle_Visita] ([Id_Detalle_Visita])
+);
